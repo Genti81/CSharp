741313e5664e1285f32c8b4c4bee011b5b136a69
Gentrit Sahiti <genti_student@outlook.com>
2017-09-11 14:29:56 +0200

    Added File Text.

---
+++ MultiplierTestsLib/FileConversiontests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiplierTestsLib
+{
+    [TestClass]
+    public class FileConversionTests
+    {
+        [TestMethod]
+        public void gentrit_sahiti_1981()
+        {
+            var sut = new FileConverter();
+            string actual = sut.Transform("gentrit;sahiti;1981");
+            Assert.AreEqual("GENTRIT SAHITI ÄR 36 ÅR GAMMAL", actual);
+        }
+        [TestMethod]
+        public void noel_sahiti_2014()
+        {
+            var sut = new FileConverter();
+            string actual = sut.Transform("noel;sahiti;2014");
+            Assert.AreEqual("NOEL SAHITI ÄR 3 ÅR GAMMAL", actual);
+        }
+        [TestMethod]
+        public void lina_sahiti_1985()
+        {
+            var sut = new FileConverter();
+            string actual = sut.Transform("lina;sahiti;1985");
+            Assert.AreEqual("LINA SAHITI ÄR 32 ÅR GAMMAL", actual);
+        }
+    }
+}
