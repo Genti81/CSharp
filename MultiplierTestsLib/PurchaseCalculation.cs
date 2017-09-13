cf2e4cf18a7bb4ed95de6f2c6c7deb59885a5ca9
Gentrit Sahiti <genti_student@outlook.com>
2017-09-13 10:22:35 +0200

    Added ListHandler.cs and ListHandlerTests.cs to Project

---
+++ MultiplierTestsLib/PurchaseCalculation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MultiplierTestsLib
+{
+    internal class PurchaseCalculation
+    {
+        public PurchaseCalculation()
+        {
+        }
+
+        internal object Execute(int qty, decimal amount, bool withVAT)
+        {
+            if (withVAT)
+                return qty * amount * 1.25M;
+            else
+                return qty * amount;
+        }
+    }
+}
