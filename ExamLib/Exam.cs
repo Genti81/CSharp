c234316f8190af4a789f3a8afe448ea4e09c221d
Gentrit Sahiti <genti_student@outlook.com>
2017-09-20 10:46:26 +0200

    Added projects ExamTestsLib and ExamLib

---
+++ ExamLib/Exam.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamLib
+{
+    public class Exam
+    {
+
+        private string Ämne;
+
+        Dictionary<Student, string> StudentToDictionary = new Dictionary<Student, string>();
+        List < Student > StudentsList = new List<Student>();
+
+        public Exam(string ämne)
+        {
+            this.Ämne = ämne;
+        }
+
+        public void Assign(Student student)
+        {
+            StudentToDictionary.Add(student, "G");
+            StudentsList.Add(student);
+        }
+
+        public void Grade(Student student, string betyg)
+        {
+            StudentToDictionary[student] = betyg;
+        }
+
+        public void AreEqual(String statistics)
+        {
+            
+        }
+
+        public Dictionary<string, int> GenerateStatistics()
+        {
+            Dictionary<string, int> GenStatistik = new Dictionary<string, int>();
+
+            GenStatistik.Add("IG", 0);
+            GenStatistik.Add("G", 0);
+            GenStatistik.Add("VG", 0);
+
+            foreach (var student in StudentToDictionary)
+            {
+                GenStatistik[student.Value]++;
+            }
+            return GenStatistik;
+        }
+    }
+}
---
+++ ExamTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExamTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExamTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("159aabb3-4b3d-4a0e-a50a-b2ace50f4fd2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
