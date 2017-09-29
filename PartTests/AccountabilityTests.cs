0e594da17d28db44e148a6d111cc6084a64813e3
Gentrit Sahiti <genti_student@outlook.com>
2017-09-29 10:31:04 +0200

    Updated

---
+++ PartTests/AccountabilityTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parter;
+
+namespace PartTests
+{
+
+    [TestClass]
+    public class AccountabilityTests
+    {
+
+        [TestMethod]
+        public void CoorectToString()
+        {
+            var gentritsahiti =
+                new Part("Gentrit Sahiti", "19810927-8898");
+            var someCompany = new Part("Company AB", "557616-6324");
+            var sut = new Accountability("VD", gentritsahiti, someCompany);
+            Assert.AreEqual("Gentrit Sahiti, VD på Company AB", sut.ToString());
+        }
+    }
+}
---
+++ Parter/Accountability.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parter
+{
+    public class Accountability
+    {
+        public string Name;
+        public Part Responsible;
+        public Part Commissioner;
+
+        public Accountability(string Name, Part responsible, Part commissioner)
+        {
+            this.Name = Name;
+            this.Responsible = responsible;
+            this.Commissioner = commissioner;
+        }
+        public override string ToString()
+        {
+            return $"{Responsible.Name}, {Name} på {Commissioner.Name}"; 
+        }
+    }
+}
