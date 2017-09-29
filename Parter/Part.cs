e1b92c5bd3fd53599af6c6ff7c345175785375fd
Gentrit Sahiti <genti_student@outlook.com>
2017-09-29 10:01:48 +0200

    Updated Part.cs

--- Parter/Part.cs
+++ Parter/Part.cs
@@ -8,14 +8,14 @@ namespace Parter
 {
     public class Part
     {
-        public string Name;
-        public string JuridisktId;
-
         public Part(string name, string juridisktId)
         {
             this.Name = name;
             this.JuridisktId = juridisktId;
         }
+        public string Name { get; set; }
+        public string JuridisktId { get; set; }
+
         public override string ToString()
         {
             return $"{Name} ({JuridisktId})"; 
