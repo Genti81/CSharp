6ca745b1b5c42d2e80afd945a0a6fe9eee30e83f
Gentrit Sahiti <genti_student@outlook.com>
2017-10-23 10:14:06 +0200

    Cleared the Practice test with Unit test

---
+++ PracticeLib/Practice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLib
+{
+    public class Practice
+    {
+        string name;
+        int years;
+        string phone;
+
+        public string NamnString
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public int YearsString
+        {
+            get
+            {
+                return years;
+            }
+            
+        }
+
+        public string Phonestring
+        {
+            get
+            {
+                return phone;
+            }
+        }
+        public string getStringValue(string v)
+        {
+            var stringValue = v.Split(';');
+            name = stringValue[0];
+            years = DateTime.Now.Year - DateTime.Parse(stringValue[1]).Year;
+            var year = DateTime.Parse(stringValue[1]).Year;
+            phone = stringValue[2];
+
+            var gatherTotalString = $"{name} är {years} år gammal och har ett telefonnummer {phone}";
+            return gatherTotalString;
+
+        }
+    }
+}
---
+++ PracticeTestsLib/PracticeTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PracticeLib;
+
+namespace PracticeTestsLib
+{
+    [TestClass]
+    public class PracticeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Practice sut = new Practice();
+
+            var stringResult = sut.getStringValue("Mattias; 1917 - 05 - 01; 0706186120");
+            Assert.AreEqual(stringResult, $"{sut.NamnString} är {sut.Phonestring} år gammal och har ett telefonnummer {sut.Phonestring}");
+        }
+    }
+}
