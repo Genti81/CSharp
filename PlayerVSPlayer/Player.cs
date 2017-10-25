15d8adae7dd8133cea29cfd780674c26914993b3
Gentrit Sahiti <genti_student@outlook.com>
2017-10-25 17:48:12 +0200

    Updated Player game

--- PlayerVSPlayer/Player.cs
+++ PlayerVSPlayer/Player.cs
@@ -5,78 +5,131 @@ namespace PlayerVSPlayer
     internal class Player
     {
         private int Damage;
+        //private int LastDmgDone;
         private int Health;
-        private int Defense;
+        private int Shield;
+        private string Name;
+        private int critChance;
 
+        
         public Player()
         {
-            Damage = 20;
-            Health = 100;
-            Defense = 30;
+            PlayerDamage = 7;
+            PlayerHealth = 100;
+            PlayerShield = 35;
+            this.PlayerName = "Default Name";
+            this.critChance = 25;
         }
 
-        internal int GetDamage()
+        public void ShowStatus()
         {
-            return Damage;
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Player: " + PlayerName);
+            if (this.PlayerShield > 0)
+                Console.WriteLine("Player shield is: " + this.PlayerShield);
+            Console.WriteLine("Player health is:" + this.PlayerHealth);
+            //if(this.PlayerDamage > 7)
+            //Console.WriteLine(this.PlayerDamage);
+            Console.WriteLine("------------------------");
+            Console.ReadLine();
         }
-        internal void SetAttackPower(int attack)
-        {
-            this.Damage = attack;
-        }
-        internal int GetHealth()
-        {
-            return Health;
-        }
-        internal void SetHealth(int health)
-        {
-            this.Health = health;
-        }
-        internal int GetDefense()
-        {
-            return Defense;
-        }
-        internal void SetDefense(int defense)
-        {
-            this.Defense = defense;
-        }
+
         public void AttackPlayer(Player otherPlayer)
         {
-            int damage = this.GetDamage();
-            Console.WriteLine("Step 1: \n Ylli's Damage attack: \n " + GetDamage() + " Dmg");
-            Console.ReadLine();
-            // player 1 attacks player 2
-            //ylli.attack(gentrit)
-            int health2 = otherPlayer.GetHealth();
-            int defense = otherPlayer.GetDefense();
-            Console.WriteLine("Step 2: \n Gentrit's HP and Defense before attack: \n " + GetHealth() + " HP  \n " + GetDefense() + " Def");
-            Console.ReadLine();
+            int damage; 
+            if (isCrit()) // If crit
+                damage = otherPlayer.PlayerShield - (this.PlayerDamage * 2);
+            else // If not crit
+                damage = otherPlayer.PlayerShield - this.PlayerDamage;
 
-            int lostHealth = health2 - damage;
-            int newStance = defense - damage;
-            int newHealth = newStance + health2;
 
-            if (newStance < 0)
-            { 
-                otherPlayer.SetHealth(Health);
-                Console.WriteLine("Step 3: \n You have still: \n " + Health + " HP");
-                Console.ReadLine(); 
-            }
-            if(defense < newStance)
-                otherPlayer.SetHealth(newHealth);
-                Console.WriteLine("Step 3: \n You lost to: \n " + newHealth + " HP");
-                Console.ReadLine();
-
-            if (otherPlayer.Health >= newHealth)
+            if (damage < 0)// Player takes damage
             {
-                otherPlayer.SetDefense(newStance);
-                otherPlayer.SetHealth(Health);
-                Console.WriteLine($"Step 4: \n Ylli attack Gentrit. \n Gentrit has: \n " + otherPlayer.Health + " HP \n " + newStance + " Def");
-                Console.ReadLine();
-            } else if (otherPlayer.Health < newHealth)
-                Console.WriteLine($"Step 4: \n Ylli attack Gentrit. \n Gentrit lost to: \n " + otherPlayer.GetHealth() + " HP \n" + " Defense depleted!");
-                Console.ReadLine();
+                otherPlayer.ChangeHealth(damage);
+                otherPlayer.PlayerShield = 0;
+            }
+            else // Only shield damage
+                otherPlayer.PlayerShield = damage;
+        }
+        public string PlayerName
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+
+        private int PlayerDamage
+        {
+            get
+            {
+                return this.Damage;
+            }
+            set
+            {
+                this.Damage = value;
+            }
+        }
+
+        private int PlayerHealth
+        {
+            get
+            {
+                return this.Health;
+            }
+            set
+            {
+                this.Health = value;
+            }
+        }
+
+        private int PlayerShield
+        {
+            get
+            {
+                return this.Shield;
+            }
+            set
+            {
+                this.Shield = value;
+            }
+        }
 
 
+        private bool isCrit()
+        {
+            Random RandomNumber = new Random();
+            return RandomNumber.Next(1, 100) <= this.critChance;
+        }
+
+
+        
+        // Skapa en metod som ändrar Hp med angivet input
+        // public ---- ChangeHealth(Int ----)
+        private void ChangeHealth(int ChangeValueOfHP)
+        {
+            //Hämta det orginal hp variabeln och addera hp med Change...variabeln till det nya
+            var newresult = this.PlayerHealth + ChangeValueOfHP;
+            this.PlayerHealth = newresult;
+        }
+
+        public bool IsDead()
+        {
+            // Lever personen?
+            // Returnera true eller false
+
+            //Ett booliskt uttryck antar alltid värdet true eller false
+            return this.PlayerHealth < 1;
         }
     }
 }
+
+
+//int damageResult = otherPlayer.GetShield() - this.GetDamage();
+//this.isCrit();
+//otherPlayer.ChangeHealth(damageResult);
+//damage = isCrit() ? otherPlayer.PlayerShield - (this.PlayerDamage * 2) : otherPlayer.PlayerShield - this.PlayerDamage;
--- PlayerVSPlayer/Program.cs
+++ PlayerVSPlayer/Program.cs
@@ -11,11 +11,25 @@ namespace PlayerVSPlayer
         static void Main(string[] args)
         {
             var ylli = new Player();
+            ylli.PlayerName = "Ylli";
             var gentrit = new Player();
+            gentrit.PlayerName = "Gentrit";
 
-            ylli.SetAttackPower(20);
-            ylli.AttackPlayer(gentrit);
 
+            while (!ylli.IsDead() && !gentrit.IsDead())
+            {
+                ylli.AttackPlayer(gentrit);
+                gentrit.AttackPlayer(ylli);
+
+                ylli.ShowStatus();
+                gentrit.ShowStatus();
+
+            }
+            if (ylli.IsDead())
+                Console.WriteLine("Ylli died");
+            if (gentrit.IsDead())
+                Console.WriteLine("Gentrit died");
+                Console.ReadLine();
         }
     }
 }
